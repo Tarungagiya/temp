c82e19de2f2e05d8a1168867220b0be9210f3d22
Tarun Gagiya <tarungagiya074@gmail.com>
2025-06-13 08:10:47 +0530

    Add golden VPC module

---
+++ modules/vpc/variables.tf
@@ -0,0 +1,27 @@
+variable "project" {
+  description = "Project name for tagging and resource naming"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name (dev, stage, prod)"
+  type        = string
+  default     = "dev"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "az_count" {
+  description = "Number of Availability Zones to span"
+  type        = number
+  default     = 3
+}
+
+variable "tags" {
+  description = "Additional tags to apply to all resources"
+  type        = map(string)
+  default     = {}
+}
