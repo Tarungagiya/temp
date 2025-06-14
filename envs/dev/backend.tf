d7a782194d153f32908843ba1a01da4740559a23
Tarun Gagiya <tarungagiya074@gmail.com>
2025-06-14 09:44:57 +0530

    final upadate

---
+++ envs/dev/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "your-company-terraform-state-00107456"
+    key            = "envs/dev/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+    encrypt        = true
+  }
+}
--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,6 +1,29 @@
+module "oidc" {
+  source = "../../modules/oidc"
+}
+
 module "vpc" {
   source      = "../../modules/vpc"
   project     = "eagerminds"
   environment = "dev"
   vpc_cidr    = "10.42.0.0/16"
 }
+# provider "aws" {
+#   region = "us-east-1"
+# }
+
+# resource "aws_s3_bucket" "tf_state" {
+#   bucket        = "your-company-terraform-state-00107456"
+#   force_destroy = true
+# }
+
+# resource "aws_dynamodb_table" "tf_lock" {
+#   name         = "terraform-lock"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
+
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
